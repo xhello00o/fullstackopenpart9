9c73e605a81cb33863e6006a61e7ce6d16f1cb4b
xhello00o <133972573+xhello00o@users.noreply.github.com>
2023-08-05 19:27:12 +0800

    d

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,34 +1,34 @@
 const calculateBmi =( centiheight: number,weight:number):string=> {
 
-    const metreheight = centiheight /100
+    const metreheight = centiheight /100;
 
-    const bmi =  weight / (metreheight**2)
+    const bmi =  weight / (metreheight**2);
 
     if (bmi < 16 ){
-        return "Underweight (Severe thinness)"
+        return "Underweight (Severe thinness)";
     } else if (bmi<17){
-        return "Underweight (Moderate thinness)"
+        return "Underweight (Moderate thinness)";
     } else if (bmi<18.5) {
-        return "Underweight (Mild thinness)"
+        return "Underweight (Mild thinness)";
     } else if (bmi<25) {
-        return "Normal range"
+        return "Normal range";
     } else if (bmi<30) {
-        return "Overweight (Pre-obese)"
+        return "Overweight (Pre-obese)";
     } else if (bmi<35) {
-        return "Obese (Class I)"
+        return "Obese (Class I)";
     } else if (bmi<40) {
-        return "Obese (Class II)"
+        return "Obese (Class II)";
     } else {
-        return "Obese (Class III)"
+        return "Obese (Class III)";
     }
 
-}
+};
 
-export default calculateBmi
+export default calculateBmi;
 
-const centiheight = Number(process.argv[2])
-const weight = Number(process.argv[3])
+const centiheight = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
 
-console.log( calculateBmi(centiheight,weight))
+console.log( calculateBmi(centiheight,weight));
 
 
